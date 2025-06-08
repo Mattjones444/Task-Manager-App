9339bda88260109193aa12c35fab3ffe2970781c
Matt Jones <mattjones@macbookpro.home>
2025-06-08 20:48:41 +0100

    Added functionality for user to view their own tasks

--- taskmanager/src/main/java/com/taskmanager/repository/TaskRepository.java
+++ taskmanager/src/main/java/com/taskmanager/repository/TaskRepository.java
@@ -1,8 +1,13 @@
 package com.taskmanager.repository;
+import com.taskmanager.model.AppUser;
 import com.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUser(AppUser user);
+
     
 }
 
